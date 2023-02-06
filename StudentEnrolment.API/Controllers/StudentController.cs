e6420a5b6dc976f7cc19c25e824206ea782e06a4
Anudeep Jami <anudeep.jami@gmail.com>
2023-02-06 21:07:40 +0000

    changes

--- StudentEnrolment.API/Controllers/StudentController.cs
+++ StudentEnrolment.API/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using StudentEnrolment.API.Models.Student;
+using StudentEnrolment.API.Models.Response;
 using StudentEnrolment.API.Services;
 
 namespace StudentEnrolment.API.Controllers;
@@ -12,10 +14,63 @@ public class StudentController : ControllerBase
     [HttpGet]
     public List<StudentDetails> GetAll()
     {
-        ReadJSON readJson = new ReadJSON();
-        List<StudentDetails> studentDetails = new List<StudentDetails>();
-        studentDetails = readJson.ReadStudentJSON();
-        return studentDetails;
+        try
+        {
+            ReadJSON readJson = new ReadJSON();
+            List<StudentDetails> studentDetails = new List<StudentDetails>();
+            studentDetails = readJson.ReadStudentJSON();
+            return studentDetails;
+        }
+        catch (Exception ex) {
+            return new List<StudentDetails>();
+        }
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public StudentDetails GetStudentById(int id)
+    {
+        try
+        {
+            int i = id;
+            ReadJSON readJson = new ReadJSON();
+            List<StudentDetails> studentDetails = new List<StudentDetails>();
+            studentDetails = readJson.ReadStudentJSON();
+            StudentDetails student = (from stud in studentDetails where stud.StudentId == id select stud).First();
+            return student;
+        }
+        catch (Exception ex) {
+            return new StudentDetails();
+        }
+
+    }
+
+    [HttpPost]
+    [Route("{id:int}")]
+    public Response UpdateStudentById(
+        [FromRoute] int id,
+        [FromBody] UpdateStudentDetails studentUpdate
+    )
+    {
+        try
+        {
+            int i = id;
+            ReadJSON readJson = new ReadJSON();
+            List<StudentDetails> studentDetails = new List<StudentDetails>();
+            studentDetails = readJson.ReadStudentJSON();
+            StudentDetails student = (from stud in studentDetails where stud.StudentId == id select stud).First();
+            return new Response {
+                message = "Student Sucessfully Updated"
+            };
+        }
+        catch (Exception ex)
+        {
+            return new Response
+            {
+                message = ex.Message
+            };
+        }
+
     }
 
 }
---
+++ StudentEnrolment.API/Models/Student/UpdateStudentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StudentEnrolment.API.Models.Student
+{
+	public class UpdateStudentDetails
+	{
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public string HomeOrOverseas { get; set; }
+
+    }
+}
+
